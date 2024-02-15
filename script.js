47ca68651efd9ace30e82fad437d5ab4472f44fe
AmarN7 <amardalavi63@gmail.com>
2024-02-15 13:49:15 +0530

    updated

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ async function fetchweatherData (cityName){
    let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7b3734957f6423ba0121990597d62d3b`
    const response = await fetch (url)
    const data = await response.json();
-   console.log(data.name)
-
+   let city = document.getElementById("city");
+   city.innerHTML = data.name;
+   let windSpeed = document.getElementById("wind_speed");
+   windSpeed.innerHTML = data.wind.speed + "km/hr";
+   let humidity = document.getElementById("humidity");
+   humidity.innerHTML = data.main.humidity+"g/kg";
+   let temp = document.getElementById("temp");
+   temp.innerHTML = ((data.main.temp)-273.15)+"°C";
+   console.log(data);
 }
-
-function fetchcity(){ 
+function fetchCity(){ 
     let cityName = document.getElementById("city_name");
     if(cityName.value===""){ 
 
@@ -14,7 +20,7 @@ function fetchcity(){
      }
      else{
          fetchweatherData(cityName.value);
-         cityName.value=""
+         cityName.value="";
       }
 
     }
